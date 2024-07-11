95a40b1803ff5ead34b6bf69bf06a07e28c07bb4
SaberHardy <samirsamado5@gmail.com>
2024-07-10 20:23:09 -0700

    upload the project

---
+++ src/main/java/com/course_api/course_api/CourseApiApplication.java
@@ -0,0 +1,11 @@
+package com.course_api.course_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/course_api/course_api/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course_api.course_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
